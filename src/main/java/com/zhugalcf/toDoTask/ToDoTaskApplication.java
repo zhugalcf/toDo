e72c76d2e4fdc97f2f7659707f821f45cfff81b0
zhugalcf <zhugalcf@gmail.com>
2023-12-15 11:36:30 +0300

    Project creation

---
+++ src/main/java/com/zhugalcf/toDoTask/ToDoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zhugalcf.toDoTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhugalcf/toDoTask/ToDoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhugalcf.toDoTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
